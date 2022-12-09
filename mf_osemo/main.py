c8f4a946b8d3e476fd681c9b12dcef5dbd66d81f
Jungtaek Kim <jungtaek.kim@pitt.edu>
2022-12-09 17:42:21 -0500

    Clean up codes

--- mf_osemo/main.py
+++ mf_osemo/main.py
@@ -26,6 +26,7 @@ functions = [mfbranin, mfCurrin]
 experiment_name = 'branin_Currin_2'
 cost = np.array([[1, 10], [1, 10]])
 
+num_X = 1000
 dim = 2
 num_functions = len(functions)
 num_fidelities = cost.shape[1]
@@ -44,12 +45,12 @@ np.random.seed(seed)
 sample_number = 1
 referencePoint = [1e5] * num_functions
 bound = [0, 1]
-X = np.random.uniform(bound[0], bound[1],size=(1000, dim))
+X = np.random.uniform(bound[0], bound[1], size=(num_X, dim))
 
 # Create data from functions
 y = [[] for i in range(0, num_functions)]
 
-for i in range(len(functions)):
+for i in range(0, num_functions):
     for m in range(0, num_fidelities):
         for xi in X:
             y[i].append(functions[i](xi, dim, m))
@@ -57,17 +58,18 @@ for i in range(len(functions)):
 y = [np.asarray(y[i]) for i in range(len(y))]
 
 # Initial setting
-size = np.size(X, 0)
 M = [len(i) for i in cost]
 fidelity_iter = [np.array([fir_num for j in range(M[i]-1)]+[1]) for i in range(len(M))]
 #fidelity_iter = [np.array([fir_num, fir_num, 1]),np.array([fir_num, 0])]
+
 total_cost = sum([sum([(float(cost[i][m])/cost[i][M[i]-1])*fidelity_iter[i][m]  for m in range(M[i])]) for i in range(len(M))])
+
 allcosts=[total_cost]
-candidate_x = [np.c_[np.zeros(size), x] for i in range(len(functions))]
+candidate_x = [np.c_[np.zeros(num_X), X] for i in range(0, num_functions)]
 
 for i in range(0, num_functions):
     for m in range(1, M[i]):
-        candidate_x[i] = np.r_[candidate_x[i], np.c_[m*np.ones(size), x]]
+        candidate_x[i] = np.r_[candidate_x[i], np.c_[m*np.ones(num_X), X]]
 
 # Kernel configuration
 kernel_f = 1. * RBF(length_scale=1, length_scale_bounds=(1e-3, 1e2))
@@ -99,10 +101,10 @@ for i in range(0, num_functions):
     temp0=[]
 
     for m in range(M[i]):
-        temp0=temp0+list(np.random.randint(size*m, size*(m+1), fidelity_iter[i][m]))
+        temp0=temp0+list(np.random.randint(num_X * m, num_X * (m + 1), fidelity_iter[i][m]))
 
     GP_index.append(np.array(temp0))
-#    GP_index.append(np.random.randint(0, size, fir_num))
+#    GP_index.append(np.random.randint(0, num_X, fir_num))
     func_samples.append([])
     acq_funcs.append([])
 
@@ -117,7 +119,7 @@ for j in range(0, 100):
         for i in range(len(functions)):
             GPs[i].fit(candidate_x[i][GP_index[i].tolist()], y[i][GP_index[i].tolist()])
             GP_mean[i], GP_std[i], cov[i] = GPs[i].predict(candidate_x[i])
-#            print("Inference Highest fidelity",GP_mean[i][x_best_index+size*(M[i]-1)])
+#            print("Inference Highest fidelity",GP_mean[i][x_best_index+num_X*(M[i]-1)])
 
     else:
         for i in range(len(functions)):
@@ -126,22 +128,22 @@ for j in range(0, 100):
     #################################################################
     for i in range(len(functions)):
         if fidelity_iter[i][M[i]-1] > 0:
-            y_max[i] = np.max(y[i][GP_index[i][GP_index[i]>=(M[i]-1)*size]])
+            y_max[i] = np.max(y[i][GP_index[i][GP_index[i] >= (M[i] - 1) * num_X]])
         else:
-            y_max[i] = GP_mean[i][(M[i]-1)*size:][np.argmax(GP_mean[i][(M[i]-1)*size:]+GP_std[i][(M[i]-1)*size:])]
+            y_max[i] = GP_mean[i][(M[i] - 1) * num_X:][np.argmax(GP_mean[i][(M[i] - 1) * num_X:]+GP_std[i][(M[i] - 1) * num_X:])]
 
         # Acquisition function calculation
     for i in range(len(functions)):
         if str_approximation == 'NI':
-            MFMES[i] = MFBO.MultiFidelityMaxvalueEntroySearch_NI(GP_mean[i], GP_std[i], y_max[i], GP_index[i], M[i], cost[i], size, cov[i],RegressionModel=GPs[i],sampling_num=sample_number)
+            MFMES[i] = MFBO.MultiFidelityMaxvalueEntroySearch_NI(GP_mean[i], GP_std[i], y_max[i], GP_index[i], M[i], cost[i], num_X, cov[i],RegressionModel=GPs[i],sampling_num=sample_number)
         else:
-            MFMES[i] = MFBO.MultiFidelityMaxvalueEntroySearch_TG(GP_mean[i], GP_std[i], y_max[i], GP_index[i], M[i], cost[i],size,RegressionModel=GPs[i],sampling_num=sample_number)
+            MFMES[i] = MFBO.MultiFidelityMaxvalueEntroySearch_TG(GP_mean[i], GP_std[i], y_max[i], GP_index[i], M[i], cost[i], num_X, RegressionModel=GPs[i],sampling_num=sample_number)
 
         func_samples[i]=MFMES[i].Sampling_RFM()
     max_samples=[]
 
     for i in range(sample_number):
-        front=[[-1*func_samples[k][l][i] for k in range(len(functions))] for l in range(size)] 
+        front=[[-1*func_samples[k][l][i] for k in range(len(functions))] for l in range(num_X)] 
         ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(points = front)
         cheap_pareto_front=[front[K] for K in ndf[0]]
         maxoffunctions=[-1*min(f) for f in list(zip(*cheap_pareto_front))]
@@ -151,11 +153,11 @@ for j in range(0, 100):
     for i in range(len(functions)):
         acq_funcs[i]=MFMES[i].calc_acq(np.asarray(max_samples[i]))
     #result[0]values of acq and remaining are the fidelity of each function 
-    result=np.zeros((size,len(functions)+1))
-    for k in range(size):
+    result=np.zeros((num_X, len(functions)+1))
+    for k in range(num_X):
         temp=[]
         for i in range(len(functions)):
-            temp.append([acq_funcs[i][k+m*size] for m in range(M[i])])
+            temp.append([acq_funcs[i][k + m * num_X] for m in range(M[i])])
         indecies=list(itertools.product(*[range(len(x)) for x in temp]))
         values_costs=[sum([float(cost[i][m])/cost[i][M[i]-1] for i,m in zip(range(len(functions)),index)]) for index in indecies]
         values=[float(sum(AF))/i for AF,i in zip(list(itertools.product(*temp)),values_costs)]
@@ -165,11 +167,11 @@ for j in range(0, 100):
             result[k][i+1]=indecies[max_index][i]
     x_best_index=np.argmax(list(zip(*result))[0])
     for i in range(len(functions)):    
-        new_index=int(x_best_index+size*result[x_best_index][i+1])
+        new_index=int(x_best_index + num_X * result[x_best_index][i + 1])
         print("new_input",candidate_x[i][new_index])                
         GP_index[i] = np.r_[GP_index[i], [new_index]]
-        total_cost += float(cost[i][new_index//size])/cost[i][M[i]-1]
-        fidelity_iter[i][new_index//size] += 1
+        total_cost += float(cost[i][new_index // num_X])/cost[i][M[i]-1]
+        fidelity_iter[i][new_index // num_X] += 1
 
     cost_input_output.write(str(total_cost)+' '+str(candidate_x[i][new_index])+' '+str(np.array([y[i][new_index] for i in range(len(functions))]))+"\n")
     cost_input_output.close()
